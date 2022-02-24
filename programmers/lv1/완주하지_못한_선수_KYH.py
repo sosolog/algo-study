80f944b68e10ce57575bc5e0e4189e7163f68b43
고영훈 <96533651+YeongHun-KO@users.noreply.github.com>
2022-02-24 17:57:08 +0900

    add [프로그래머스] 완주하지 못한 선수 / KYH

---
+++ programmers/lv1/완주하지_못한_선수_KYH.py
@@ -0,0 +1,13 @@
+def solution(participant, completion):
+    hashDict = {}
+    sumHash = 0
+    
+    for p in participant:
+        hashDict[hash(p)] = p
+        sumHash += hash(p)
+        
+    for c in completion:
+        sumHash -= hash(c)
+        
+    answer = hashDict[sumHash]
+    return answer
