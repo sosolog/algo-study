297aca8359cf6e16241303183779a2253474f541
고영훈 <96533651+YeongHun-KO@users.noreply.github.com>
2022-05-03 18:30:00 +0900

    add [백준] 11399 ATM / KYH

---
+++ baekjoon/11399_ATM_KYH.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+P = list(map(int, input().split()))
+P.sort()
+answer = 0
+
+for i in range(N):
+    answer += P[i] * (N - i)
+print(answer)
+
